cbeeba15967483374b30abe56b5f8bbe1ae8e852
felenitaribeiro <felenita.ribeiro@gmail.com>
2018-09-17 16:50:11 -0300

    Codes

---
+++ src/twin_IDs.py
@@ -0,0 +1,12 @@
+import numpy as np
+import xlrd
+
+#Index ID python
+workbook = xlrd.open_workbook('/home/hardleste/Desktop/HCP_DATA_12_2017/python_scripts_ESTUDO1/List_twin_pairs.xlsx')
+sheet = workbook.sheet_by_index(0)
+
+twin_pairs_ID=[]
+for rowx in range(sheet.nrows):
+    cols = sheet.row_values(rowx)
+    twin_pairs_ID.append(cols)
+
